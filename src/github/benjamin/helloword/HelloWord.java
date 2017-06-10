89bdf648c6b0048f7a15d392898ad2c5cd93a728
Benjamin <cage_ben@sina.com>
2017-06-10 14:42:16 +0800

    first commit

---
+++ src/github/benjamin/helloword/HelloWord.java
@@ -0,0 +1,22 @@
+package github.benjamin.helloword;
+
+public class HelloWord {
+
+  public static void main(String[] args) {
+//	System.err.println("你好 java 我要学会你");
+/*	  System.out.println("你好 java 我要学会你");
+	  System.out.println("《java 入门到精通》");
+	  System.out.println("是我学习java的最佳书籍");*/
+  for (int i = 0; i < 10; i++) {
+  if(i==7)
+  {
+	  continue;
+  }
+
+  System.out.println("第"+i+"行数据");	
+  }
+  
+  }
+	
+
+}
